b25c732ae46573951fa7daa409eee12ccd02066c
Osman Gökmen <osmangokmenx@gmail.com>
2023-09-15 23:21:01 +0300

    add search component

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+})
+export class SearchBarComponent {
+  @Output() queryChange = new EventEmitter<any>();
+  timeout: any;
+  name = '';
+  species = '';
+  status = '';
+  gender = '';
+
+  constructor() {}
+
+  onQueryChange(): void {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.queryChange.emit({
+        name: this.name,
+        species: this.species,
+        gender: this.gender,
+        status: this.status,
+      });
+    }, 400);
+  }
+}
