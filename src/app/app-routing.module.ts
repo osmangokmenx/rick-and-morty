f47648c7ebad5d08ed2818fa8a98c69beacf6400
Osman Gökmen <osmangokmenx@gmail.com>
2023-09-16 00:10:04 +0300

    add character route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CharacterComponent } from './pages/character/character.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'character/:id', component: CharacterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
