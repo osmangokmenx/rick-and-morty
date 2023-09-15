335d4875f73ab993e1fe54162b20184d88a6b0af
Osman Gökmen <osmangokmenx@gmail.com>
2023-09-16 01:36:37 +0300

    add error handler

--- src/app/services/character/character.service.ts
+++ src/app/services/character/character.service.ts
@@ -1,24 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 import {
+  Character,
   CharacterFilter,
   CharacterResult,
 } from 'src/app/interfaces/character.interface';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
   constructor(private http: HttpClient) {}
+  private handleError() {
+    let errorMessage = 'An error occurred';
+
+    // Return an observable with a user-facing error message
+    return throwError(errorMessage);
+  }
 
   getCharacters(props: CharacterFilter) {
     const { name, status, species, gender, page } = props;
-    return this.http.get<CharacterResult>(
-      `${environment.apiUrl}?page=${page ?? 0}&name=${name ?? ''}&status=${
-        status ?? ''
-      }&species=${species ?? ''}&gender=${gender ?? ''}`
-    );
+    return this.http
+      .get<CharacterResult>(
+        `${environment.apiUrl}?page=${page ?? 0}&name=${name ?? ''}&status=${
+          status ?? ''
+        }&species=${species ?? ''}&gender=${gender ?? ''}`
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  getCharacter(id: number) {
+    return this.http
+      .get<Character>(`${environment.apiUrl}/${id}`)
+      .pipe(catchError(this.handleError));
   }
 }
